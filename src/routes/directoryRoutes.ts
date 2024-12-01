10018a6000f6d58876ca436b5eb3d8d00ca35ebb
wjatek <kontakt@wjatek.pl>
2024-12-01 20:25:09 +0100

    add endpoint for creating directories

--- src/routes/directoryRoutes.ts
+++ src/routes/directoryRoutes.ts
@@ -1,5 +1,6 @@
-import express, { Request, Response, NextFunction } from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import prisma from '../prisma/client'
+import { Directory } from '@prisma/client'
 
 const router = express.Router()
 
@@ -28,6 +29,50 @@ router.get(
       }
 
       res.json(directory)
+    } catch (err) {
+      next(err)
+    }
+  }
+)
+
+router.post(
+  '/',
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const { name, parentId } = req.body
+      const authorId = req.user.id
+
+      if (!name) {
+        res.status(400).json({ error: 'Name is required' })
+        return
+      }
+
+      const parent = await prisma.directory.findUnique({
+        where: { id: parentId },
+        include: {
+          subdirectories: true,
+        },
+      })
+
+      if (!parent) {
+        res.status(400).json({ error: 'Parent directory does not exist' })
+        return
+      }
+
+      if (parent.subdirectories.find(subdirectory => subdirectory.name === name)) {
+        res.status(400).json({ error: 'Directory already exists' })
+        return
+      }
+
+      const newDirectory = await prisma.directory.create({
+        data: {
+          name,
+          authorId,
+          parentId,
+        },
+      })
+
+      res.status(201).json(newDirectory)
     } catch (err) {
       next(err)
     }
