d836111272ddc577baa71b76f1df80dedfc76c5e
wjatek <kontakt@wjatek.pl>
2024-12-01 11:45:03 +0100

    add posts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -8,11 +8,26 @@
 import prisma from "../src/prisma/client";
 
 async function seed() {
+  await prisma.post.deleteMany({});
+  await prisma.user.deleteMany({});
+  
+  await prisma.$executeRaw`ALTER SEQUENCE "User_id_seq" RESTART WITH 1;`;
+  await prisma.$executeRaw`ALTER SEQUENCE "Post_id_seq" RESTART WITH 1;`;
+
   await prisma.user.createMany({
     data: [
       { name: "Alice" },
       { name: "Bob" },
       { name: "Charlie" },
+    ],
+  });
+
+  const users = await prisma.user.findMany();
+  await prisma.post.createMany({
+    data: [
+      { content: "Alice's first post", authorId: users[0].id },
+      { content: "Bob's first post", authorId: users[1].id },
+      { content: "Charlie's first post", authorId: users[2].id },
     ],
   });
 }
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import userRoutes from "./routes/userRoutes";
+import postRoutes from "./routes/postRoutes";
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ const app = express();
 app.use(cors());
 
 app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
