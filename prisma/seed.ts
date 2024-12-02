025655eacbed0217b92f2e0fb3bb6fa5bbea3f6a
wjatek <kontakt@wjatek.pl>
2024-12-02 12:41:30 +0100

    add generating files to seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,8 +5,70 @@
  * npx ts-node prisma/seed.ts
  */
 
-import prisma from '../src/prisma/client'
 import bcrypt from 'bcryptjs'
+import fs from 'fs'
+import path from 'path'
+import prisma from '../src/prisma/client'
+import { FILE_STORAGE_PATH } from '../src/services/fileService'
+
+const generateFile = async (
+  dir: string,
+  fileName: string,
+  sizeInMB: number
+): Promise<void> => {
+  const filePath = path.join(dir, fileName)
+
+  if (!fs.existsSync(dir)) {
+    await fs.promises.mkdir(dir, { recursive: true })
+  }
+
+  const writeStream = fs.createWriteStream(filePath)
+
+  const oneMB = 1024 * 1024
+  const sizeInBytes = sizeInMB * oneMB
+
+  const randomData = Buffer.alloc(oneMB)
+
+  let written = 0
+  while (written < sizeInBytes) {
+    if (written + oneMB > sizeInBytes) {
+      writeStream.write(randomData.slice(0, sizeInBytes - written))
+    } else {
+      writeStream.write(randomData)
+    }
+    written += oneMB
+  }
+
+  writeStream.end()
+
+  return new Promise<void>((resolve, reject) => {
+    writeStream.on('finish', () => {
+      console.log(
+        `File '${fileName}' of size ${sizeInMB}MB has been created at ${dir}`
+      )
+      resolve()
+    })
+
+    writeStream.on('error', (err) => {
+      console.error(`Error writing file: ${err.message}`)
+      reject(err)
+    })
+  })
+}
+
+const clearDirectory = async (dir: string): Promise<void> => {
+  if (fs.existsSync(dir)) {
+    const files = await fs.promises.readdir(dir)
+    for (const file of files) {
+      const filePath = path.join(dir, file)
+      const stats = await fs.promises.stat(filePath)
+      if (stats.isFile()) {
+        await fs.promises.unlink(filePath)
+      }
+    }
+    console.log(`All files removed from ${dir}`)
+  }
+}
 
 async function seed() {
   await prisma.file.deleteMany()
@@ -51,36 +113,42 @@ async function seed() {
     data: { name: "Bob's Root Directory", authorId: bob.id },
   })
 
-  await prisma.file.create({
+  clearDirectory(FILE_STORAGE_PATH)
+
+  const file1 = await prisma.file.create({
     data: {
       name: 'alice music.mp3',
       authorId: alice.id,
       directoryId: aliceRootDir.id,
     },
   })
+  await generateFile(FILE_STORAGE_PATH, file1.id, 12)
 
-  await prisma.file.create({
+  const file2 = await prisma.file.create({
     data: {
       name: 'alice resume.pdf',
       authorId: alice.id,
       directoryId: aliceSubDir.id,
     },
   })
+  await generateFile(FILE_STORAGE_PATH, file2.id, 15)
 
-  await prisma.file.create({
+  const file3 = await prisma.file.create({
     data: {
       name: "Bob's File 1",
       authorId: bob.id,
       directoryId: bobRootDir.id,
     },
   })
+  await generateFile(FILE_STORAGE_PATH, file3.id, 10)
 
-  await prisma.file.create({
+  const file4 = await prisma.file.create({
     data: {
-      name: "file in root directory.txt",
+      name: 'file in root directory.txt',
       authorId: bob.id,
     },
   })
+  await generateFile(FILE_STORAGE_PATH, file4.id, 102)
 }
 
 seed()
