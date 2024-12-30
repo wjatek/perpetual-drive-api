1acf68c75575aca81d4bcd67a7db2ee2fd539484
wjatek <kontakt@wjatek.pl>
2024-12-30 19:42:54 +0100

    add default cors origin

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ import cookieParser from 'cookie-parser'
 dotenv.config()
 
 const PORT = process.env.PORT || '3000'
-const CORS_ORIGIN = process.env.CORS_ORIGIN || ''
+const CORS_ORIGIN = process.env.CORS_ORIGIN || 'http://localhost:3000'
 
 const app = express()
 
